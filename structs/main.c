59bc62f1742e391477c3fcd77c3b3ba31f125e82
Antonio <35410603+redyz13@users.noreply.github.com>
2021-12-06 23:21:10 +0100

    modified:   main.c

--- structs/main.c
+++ structs/main.c
@@ -2,24 +2,21 @@
 #include "persona.h"
 
 int main() {
-    persona fabio, giovannino;
-    persona *fp, *gp;
-    
+    persona *fabio = NULL, *giovannino = NULL;
+
     printf("--- Fabbione ---\n\n");
 
-    fp = &fabio;
-    fp = alloca_persona(fp);
-    fabio = build_persona("Fabio", "Fabbione", 20);
+    fabio = alloca_persona(fabio);
+    *fabio = build_persona("Fabio", "Fabbione", 20);
 
-    print_persona(fabio);
+    print_persona(*fabio);
 
     printf("\n--- Giovannino ---\n\n");
 
-    gp = &giovannino;
-    gp = alloca_persona(gp);
-    giovannino = build_persona("Giovannino", "La Marca", 19);
+    giovannino = alloca_persona(giovannino);
+    *giovannino = build_persona("Giovannino", "La Marca", 19);
 
-    print_persona(giovannino);
+    print_persona(*giovannino);
 
     return 0;
 }
